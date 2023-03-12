a969089b0b0efddd8635f8c3d6a102cbcd40ab56
CrisLoony <125072750+CrisLoony@users.noreply.github.com>
2023-03-11 23:21:22 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,32 @@
+const botoesCarrossel = document.querySelectorAll('.botao');
+const imagens = document.querySelectorAll('.imagem')
+
+botoesCarrossel.forEach((botao, indice) => {
+    botao.addEventListener('click', () => {
+
+        desativarBotaoSelecionado();
+
+        selecionarBotao(botao);
+
+        esconderImagemAtiva();
+
+        ativarImagemCarrossel(indice);
+    })
+})
+function ativarImagemCarrossel(indice) {
+    imagens[indice].classList.add('ativa');
+}
+
+function selecionarBotao(botao) {
+    botao.classList.add('selecionado');
+}
+
+function esconderImagemAtiva() {
+    const imagemAtiva = document.querySelector('.ativa');
+    imagemAtiva.classList.remove('ativa');
+}
+
+function desativarBotaoSelecionado() {
+    const botaoSelecionado = document.querySelector('.selecionado');
+    botaoSelecionado.classList.remove('selecionado');
+}
